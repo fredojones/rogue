85de8250f60340e68dc4c52e993f7489e30d194e
Joe D. Bentley <joebentley10@gmail.com>
2015-05-13 21:12:05 +0100

    Removed some obsolete code

--- rogue/player.py
+++ rogue/player.py
@@ -2,20 +2,15 @@ from .entity import Entity
 from .tile import Tile
 
 class Player(Entity):
-    """ Player class controlled by the user.
-
-    Attributes:
-    items -- list containing items held by player """
+    """ Player class controlled by the user. """
 
     def __init__(self, x, y):
-        self.items = []
         super(Player, self).__init__(x, y, tile=Tile.player, solid=True)
 
     def attack_move(self, x, y, world):
         """ Moves as normal but also attacks enemy if enemy is in square
             being moved to.
         """
-
         # Deal damage if moving into enemy
         entity = world.get_entity_at(x, y)
         if entity is not None and entity.tag == 'enemy':
@@ -47,9 +42,6 @@ class Player(Entity):
         game.camera.center_on(self, game.world)
 
         super().update(game, key)
-
-    def add_item(self, item):
-        self.items.append(item)
 
     @property
     def attack(self):
