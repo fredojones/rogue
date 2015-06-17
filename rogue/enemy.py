637971201795f4fd41e6e3ee5a8ef3ea385a27be
Joe D. Bentley <joebentley10@gmail.com>
2015-06-17 13:34:04 +0100

    Enemies now move towards the player each frame

--- rogue/enemy.py
+++ rogue/enemy.py
@@ -1,3 +1,4 @@
+import random
 from .entity import Entity
 from .tile import Tile
 
@@ -7,4 +8,25 @@ class Enemy(Entity):
     def __init__(self, x=0, y=0, health=10, tile=Tile.enemy):
         super().__init__(x=x, y=y, health=health, tile=tile, solid=True,
                          name='goblin', tag='enemy')
+
+    def update(self, game):
+        """ Update the enemy, will move towards the player randomly-ish.
+        
+        Enemy will move directly towards the player with some chance.
+        """
+
+        if random.random() > 0.3:
+            if game.player.x > self.x:
+                self.move(self.x + 1, self.y, game.world)
+        if random.random() > 0.3:
+            if game.player.x < self.x:
+                self.move(self.x - 1, self.y, game.world)
+        if random.random() > 0.3:
+            if game.player.y > self.y:
+                self.move(self.x, self.y + 1, game.world)
+        if random.random() > 0.3:
+            if game.player.y < self.y:
+                self.move(self.x, self.y - 1, game.world)
+
+        super().update(game)
 
--- tests/test_enemy.py
+++ tests/test_enemy.py
@@ -1,8 +1,49 @@
 import pytest
-from rogue.enemy import Enemy
+from rogue.enemy  import Enemy
+from rogue.world  import World
+from rogue.player import Player
+from rogue.camera import Camera
+
+class Dummy_Game(object):
+    pass
+
+@pytest.fixture
+def camera():
+    return Camera()
+
+@pytest.fixture
+def player():
+    return Player(x=100, y=102)
+
+@pytest.fixture
+def world():
+    return World(width=200, height=200)
 
 @pytest.fixture
 def enemy():
     return Enemy(x=100, y=100)
 
+@pytest.fixture
+def game():
+    game = Dummy_Game()
+    game.world = world()
+    game.camera = camera()
+    game.player = player()
+    game.enemy = enemy()
+    game.world.add_entity(game.player)
+    game.world.add_entity(game.enemy)
+    return game
 
+def test_enemy_moves_towards_player(game):
+    game.world.update(game)
+    assert game.enemy.x == 100
+    assert game.enemy.y == 101
+
+    game.player.x = 102
+    game.player.y = 100
+
+    game.world.update(game)
+
+    assert game.enemy.x == 101
+    assert game.enemy.y == 100
+    
